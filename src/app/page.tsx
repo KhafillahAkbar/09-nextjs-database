6c087e5f393d33551dfd64174e608330a79ed69e
KhafillahAkbar <fillaakbar095@gmail.com>
2024-06-06 23:53:23 +0700

    W09 : Jawaban Soal praktikum

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,22 +2,30 @@ import { Card } from './components/atoms/molecules/card';
 import RevenueChart from './components/atoms/molecules/revenue-chart';
 import LatestInvoices from './components/atoms/molecules/latest-invoices';
 import { lusitana } from '@/app/components/atoms/fonts';
+import { fetchCardData } from '@/model/query';
 
 export default async function Page() {
+  const {
+    numberOfCustomers,
+    numberOfInvoices,
+    totalPaidInvoices,
+    totalPendingInvoices,
+  } = await fetchCardData();
+
   return (
     <main>
       <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
         Dashboard
       </h1>
       <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
-        {/* <Card title="Collected" value={totalPaidInvoices} type="collected" /> */}
-        {/* <Card title="Pending" value={totalPendingInvoices} type="pending" /> */}
-        {/* <Card title="Total Invoices" value={numberOfInvoices} type="invoices" /> */}
-        {/* <Card
+        { <Card title="Collected" value={totalPaidInvoices} type="collected" /> }
+        { <Card title="Pending" value={totalPendingInvoices} type="pending" /> }
+        { <Card title="Total Invoices" value={numberOfInvoices} type="invoices" /> }
+        { <Card
           title="Total Customers"
           value={numberOfCustomers}
           type="customers"
-        /> */}
+        /> }
       </div>
       <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
         { <RevenueChart />}
